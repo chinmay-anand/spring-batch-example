4739f53f7c7652395b54be361c68e0cf4d7f15e4
chinmay-anand <34304780+chinmay-anand@users.noreply.github.com>
2020-05-01 03:31:24 +0530

    Powershell script to build and  rerun the same job by passing a dummy current time parameter
    Need to run the following powershell command before running the script
    *   Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
    
    Else the powershell script invocation gives error "PowerShell script not digitally signed"

---
+++ spring-batch/spring-batch-1/rerun_delivery_job.ps1
@@ -0,0 +1,8 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+$CURRENT_DATE = GET-DATE -Format "yyyy/MM/dd"
+$CURRENT_TIME = get-date -format "hh:mm:ss"
+$LESSON = pwd | Select-Object | %{$_.ProviderPath.Split("\")[-1]}
+mvn clean package "-Dmaven.test.skip=true";
+$JAR_PATH = Resolve-Path ./target/spring*.jar
+java -jar $JAR_PATH "item=shoes" "packing_date(date)=$CURRENT_DATE" "lesson=$LESSON" "time=$CURRENT_TIME";
+pause;
