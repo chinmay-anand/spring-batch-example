a3d2ea64a34bcd7b7645c73552dcf16ca6859867
chinmay-anand <34304780+chinmay-anand@users.noreply.github.com>
2020-05-04 23:25:49 +0530

    script parameter corrected

--- spring-batch/spring-batch-1/rerun_flowers_job.ps1
+++ spring-batch/spring-batch-1/rerun_flowers_job.ps1
@@ -4,5 +4,6 @@ $CURRENT_TIME = get-date -format "hh:mm:ss"
 $LESSON = pwd | Select-Object | %{$_.ProviderPath.Split("\")[-1]}
 mvn clean package "-Dmaven.test.skip=true";
 $JAR_PATH = Resolve-Path ./target/spring*.jar
-java -jar "-Dspring.batch.job.names=prepare_flowers_job" $JAR_PATH "item=shoes" "packing_date(date)=$CURRENT_DATE" "lesson=$LESSON" "time=$CURRENT_TIME" "type=args[0]";
+### Usage: "rerun_flowers_job.ps1 roses"  -- when "roses" is passed to ps script the remove_thorns step gets triggered from Spring Batch job
+java -jar "-Dspring.batch.job.names=prepare_flowers_job" $JAR_PATH "item=shoes" "packing_date(date)=$CURRENT_DATE" "lesson=$LESSON" "time=$CURRENT_TIME" type=$args;
 pause;
--- spring-batch/spring-batch-1/run_flowers_job.ps1
+++ spring-batch/spring-batch-1/run_flowers_job.ps1
@@ -4,5 +4,6 @@ $CURRENT_TIME = get-date -format "hh:mm:ss"
 $LESSON = pwd | Select-Object | %{$_.ProviderPath.Split("\")[-1]}
 mvn clean package "-Dmaven.test.skip=true";
 $JAR_PATH = Resolve-Path ./target/spring*.jar
-java -jar "-Dspring.batch.job.names=prepare_flowers_job" $JAR_PATH "item=shoes" "packing_date(date)=$CURRENT_DATE" "lesson=$LESSON" "type=args[0]";
+### Usage: "run_flowers_job.ps1 roses"  -- when "roses" is passed to ps script the remove_thorns step gets triggered from Spring Batch job
+java -jar "-Dspring.batch.job.names=prepare_flowers_job" $JAR_PATH "item=shoes" "packing_date(date)=$CURRENT_DATE" "lesson=$LESSON" type=args;
 pause;
